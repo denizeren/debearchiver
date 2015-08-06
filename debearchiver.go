ebea87b601d0f5e10446abcf8f747c73a0ccb667
Deniz Eren <dee.116@gmail.com>
2015-08-06 08:22:16 +0300

    Entry struct added.

--- debearchiver.go
+++ debearchiver.go
@@ -6,8 +6,31 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 )
+
+type Entry struct {
+	url     string
+	subject string
+	author  string
+}
+
+var aylar = [...]string{
+	"Ocak",
+	"Şubat",
+	"Mart",
+	"Nisan",
+	"Mayıs",
+	"Haziran",
+	"Temmuz",
+	"Ağustos",
+	"Eylül",
+	"Ekim",
+	"Kasım",
+	"Aralık",
+}
 
 func main() {
 	client := &http.Client{}
@@ -29,12 +52,37 @@ func main() {
 		fmt.Println("error", err)
 	}
 
-	parseHtml(strings.NewReader(string(contents)))
+	var entries []Entry
+	parseHtml(strings.NewReader(string(contents)), &entries)
+
+	writeFile(entries)
 }
 
-func parseHtml(r io.Reader) {
+func writeFile(entries []Entry) {
+	year, month, day := time.Now().Date()
+	filename := fmt.Sprintf("%d-%02d-%02d-%d-%s-%d-eksisozluk-debe.md", year, int(month), day, day, aylar[int(month)-1], year)
+
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	_, err = f.WriteString(fmt.Sprintf("---\nlayout: post\ntitle: %d %s %d Ekşi Sözlük Debe\n---\n\n", day, aylar[int(month)-1], year))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range entries {
+		_, err = f.WriteString(fmt.Sprintf("* [%s](http://eksisozluk.com/%s)\n", v.subject, v.url))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func parseHtml(r io.Reader, entries *[]Entry) {
 	parseEnable := false
 	tokenT := ""
+	var entry Entry
 
 	d := html.NewTokenizer(r)
 	for {
@@ -62,14 +110,21 @@ func parseHtml(r io.Reader) {
 
 			if parseEnable {
 				if token.Data == "a" && len(token.Attr) > 0 {
-					fmt.Println(token.Attr[0].Val)
+					entry.author = ""
+					entry.subject = ""
+					entry.url = token.Attr[0].Val
 				}
 			}
 			tokenT = token.Data
 
 		case html.TextToken: // text between start and end tag
 			if parseEnable && (tokenT == "span" || tokenT == "div") {
-				fmt.Println(token.Data)
+				if len(entry.subject) > 0 {
+					entry.author = token.Data
+					*entries = append(*entries, entry)
+				} else {
+					entry.subject = token.Data
+				}
 			}
 		case html.EndTagToken: // </tag>
 			tokenT = ""
